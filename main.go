fd28da5fdc67a6453cb9ba6e599d0caf18e0cadb
Rendy Kamaluddin <147012406+rendykml@users.noreply.github.com>
2025-05-09 11:58:16 +0700

    edit main.go

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ func main() {
     app := fiber.New()
 
     app.Get("/", func(c *fiber.Ctx) error {
-        return c.SendString("Hello, World 👋!")
+        return c.SendString("Hello, World 👋! by rendy")
     })
 
     app.Listen(":3000")
